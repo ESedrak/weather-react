0b40618c64e2887c34cdd5156f19977985cf5104
ESedrak <elizabeth_sedrak@hotmail.com>
2022-04-04 14:35:58 +1000

    Search engine now works/updates city

---
+++ src/CurrentWeather.js
@@ -0,0 +1,34 @@
+import React from "react";
+import FormatDate from "./FormatDate";
+
+export default function CurrentWeather(props) {
+  return (
+    <div className="CurrentWeather">
+      <h1> {props.data.city}</h1>
+      <div className="row">
+        <div className="col-3">
+          <img
+            src={props.data.iconUrl}
+            alt={props.data.description}
+            className="weather-icon"
+          />
+        </div>
+        <div className="col-5 today-temperature-units">
+          <span className="today-temperature">{props.data.temperature}</span>
+          <span className="units"> °C </span>
+        </div>
+        <div className="col-4">
+          <ul className="extra-info">
+            <li>{props.data.description}</li>
+            <li>Humidity:{props.data.humidity}%</li>
+            <li>Wind:{props.data.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+      <span className="last-updated-time">
+        Last Updated:
+        <FormatDate date={props.data.date} />
+      </span>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import ReactAnimatedWeather from "react-animated-weather";
-import FormatDate from "./FormatDate";
+import CurrentWeather from "./CurrentWeather";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function showWeather(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: Math.round(response.data.main.temp),
@@ -22,17 +22,33 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    let apiKey = "2fe015e63630ed57d2ca7047e4ab4479";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function showSearch(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function showCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Search">
         <nav className="navbar nav-bar-dark">
-          <form className="d-flex">
+          <form className="d-flex" onSubmit={showSearch}>
             <input
               className="form-control me-2 w-100"
               type="search"
               placeholder="City"
               autoComplete="off"
               autoFocus="on"
+              onChange={showCityChange}
             />
             <button className="btn btn-outline-light " type="submit">
               Search
@@ -42,42 +58,11 @@ export default function Weather(props) {
             </button>
           </form>
         </nav>
-        <div className="Today">
-          <h1> {weatherData.city}</h1>
-          <div className="row">
-            <div className="col-3">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="weather-icon"
-              />
-            </div>
-            <div className="col-5 today-temperature-units">
-              <span className="today-temperature">
-                {weatherData.temperature}
-              </span>
-              <span className="units"> °C </span>
-            </div>
-            <div className="col-4">
-              <ul className="extra-info">
-                <li>{weatherData.description}</li>
-                <li>Humidity:{weatherData.humidity}%</li>
-                <li>Wind:{weatherData.wind} km/h</li>
-              </ul>
-            </div>
-          </div>
-          <span className="last-updated-time">
-            Last Updated:
-            <FormatDate date={weatherData.date} />
-          </span>
-        </div>
+        <CurrentWeather data={weatherData} />
       </div>
     );
   } else {
-    let apiKey = "2fe015e63630ed57d2ca7047e4ab4479";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showWeather);
-
+    search();
     return (
       <div>
         <p>Loading....</p>
